a43a20167ee5df570c4a5b3964bd971fcf9ef18c
Austin Roberts <d.austin.roberts@gmail.com>
2018-07-03 14:18:02 -0700

    This adds tickets to home

--- src/components/ConfirmationQuestions.jsx
+++ src/components/ConfirmationQuestions.jsx
@@ -1,11 +1,11 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-function ConfirmationQuestions(){
+function ConfirmationQuestions(props){
   return (
     <div>
       <p>Have you gone through all the steps?</p>
-      <button>Yes</button>
+      <button onClick={props.onTroubleshootingConfirmation}>Yes</button>
     </div>
   );
 }
