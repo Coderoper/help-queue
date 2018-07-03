4003a79bb0d78fb43159e1448ba09e76dc87fbb5
Austin Roberts <d.austin.roberts@gmail.com>
2018-07-03 16:22:30 -0700

    adding a timer to the code

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -4,7 +4,6 @@ import TicketList from './TicketList';
 import NewTicketControl from './NewTicketControl';
 import Error404 from './Error404';
 import { Switch, Route } from 'react-router-dom';
-
 class App extends React.Component {
 
   constructor(props) {
@@ -14,9 +13,42 @@ class App extends React.Component {
     };
     this.handleAddingNewTicketToList = this.handleAddingNewTicketToList.bind(this);
   }
-  
+  componentDidUpdate(){
+    console.log('componentDidUpdate');
+  }
+  componentWillUpdate(){
+    console.log('componentWillUpdate');
+  }
+  shouldComponentUpdate(){
+    console.log('shouldComponentUpdate');
+    return true;
+  }
+  componentWillReceiveProps(){
+    console.log('comoponentWillReceiveProps');
+  }
+  componentWillMount(){
+    console.log('componentWillMount');
+  }
+  componentWillUnmount(){
+    console.log('componentWillUnmount');
+    clearInterval(this.waitTimeUpdateTimer);
+  }
+
+  updateTicketElapsedWaitTime(){
+    console.log('check');
+    let newMasterTicketList = this.state.masterTicketList.slice();
+    newMasterTicketList.forEach((ticket) => ticket.formattedWaitTime = (ticket.timeOpen).fromNow(true));
+    this.setState({masterTicketList: newMasterTicketList});
+  }
+  componentDidMount(){
+    console.log('componentDidMount');
+    this.waitTimeUpdateTimer = setInterval(() =>
+      this.updateTicketElapsedWaitTime(), 60000);
+  }
+
   handleAddingNewTicketToList(newTicket) {
     var newMasterTicketList = this.state.masterTicketList.slice();
+    newTicket.formattedWaitTime = (newTicket.timeOpen).fromNow(true);
     newMasterTicketList.push(newTicket);
     this.setState({masterTicketList: newMasterTicketList});
   }
@@ -34,8 +66,5 @@ class App extends React.Component {
   }
 
 }
-
-
-
 
 export default App;
